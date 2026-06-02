5b5d5b5b8e5044846a5bb3a1e984137dddef65ec
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-06-02 09:04:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trek-mapper-v30-media';
+const CACHE_NAME = 'trek-mapper-v31-live';
 const TILE_CACHE = 'trek-mapper-tiles';
 const urlsToCache = [
   './',
@@ -19,7 +19,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('[PekerjaServis] Cache dibuka v30');
+        console.log('[PekerjaServis] Cache dibuka v31');
         return cache.addAll(urlsToCache).catch(err => console.warn('[PekerjaServis] Amaran cache:', err));
       })
   );
