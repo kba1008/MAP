582688a6e3c11867f5d6e2af885d96036cff7f13
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-06-04 20:14:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyFOyW0IVmGme4U3Ang_2yeUQVKQjzgYWIoAed39tn03Zv58DwBp2eRGcUVqejeb17y/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycby-ubktG8p4F1uv8VRzSMWgZyKXPRRIS15LdnnGnfjUzFDd15bQIgAA2A70MsXUioSj/exec";
 
 const EMOJI_LIST = [
   "📍 Lokasi Biasa", "🏁 Mula/Tamat", "🚩 Bendera Merah", "🎌 Bendera Silang", "⭐ Bintang",
